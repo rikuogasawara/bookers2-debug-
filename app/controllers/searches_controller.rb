1fdb9417609c9cdc828a425dd27de4ad09a6bc07
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-03-05 21:37:28 +0000

    first commit

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,16 @@
+class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def search
+    @range = params[:range]
+    @search = params[:search]
+    @word = params[:word]
+    
+    if @range == "User"
+      @users = User.looks(@search, @word)
+    else
+      @books = Book.looks(@search, @word)
+    end
+  end
+  
+end
